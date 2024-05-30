5146d93c40cd7f6eb6064c924cac5cfb7d99e2e9
sernaailton <ailtonserna64@gmail.com>
2024-05-30 17:45:11 -0600

    Practica 6 - ionic start

---
+++ miApp2061151/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'miApp2061151',
+  webDir: 'www'
+};
+
+export default config;

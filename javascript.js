356c21f0b56aa8c586b6d5caf5181af06faf802b
sernaailton <ailtonserna64@gmail.com>
2024-05-30 16:26:36 -0600

    practica 1 y 2 - mi primer commit

---
+++ javascript.js
@@ -0,0 +1,16 @@
+var miTxt = document.getElementById("txt");
+var texto = "";
+
+function FuncionIm(){
+    texto = miTxt.value;
+    alert(texto);
+}
+
+function FuncionEl(){
+    miTxt.value = "";
+    texto = "";
+}
+
+function Cambiar(){
+
+}

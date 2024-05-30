9bef5f23a9f6776909b9f5b98ae579b75e49c9ca
sernaailton <ailtonserna64@gmail.com>
2024-05-30 17:26:39 -0600

    Practica 3 - login

--- javascript.js
+++ javascript.js
@@ -1,23 +1,38 @@
-var articulo = document.getElementById("articuloID");
-var monto = document.getElementById("montoID");
-var art = '';
-var mont = '';
-var total = 0;
+const loginBtn = document.getElementById("RegistrarBtn");
+const signUpBtn = document.getElementById("IniciarBtn");
+const continueBtn = document.getElementById("continueBt");
+const user = document.getElementById("usuarioIn");
+const password = document.getElementById("passwordIn");
+const popUp = document.getElementById("popUp");
+const msg = document.getElementById("message");
 
-var seccionTicket = document.getElementById("ticket");
-var preciototal = document.getElementById("total");
+function iniciar(){
+    if(user.value === "" || password.value === ""){
+        alert("Porfavor llene todos los campos");
+        return;
+    }
 
-function imprimir(){
-    mont = parseFloat(monto.value);
-    art = articulo.value;
-    total = total + mont;
-
-    var elemento = document.createElement("p");
-    elemento.innerHTML = art + ".... $" + mont;
-
-    seccionTicket.insertBefore(elemento, preciototal);
-    preciototal.textContent = "Total: $" + total;
-
-    articulo.value = '';
-    monto.value = '';
+    msg.innerHTML = "Inicio de sesion exitoso " + (user.value).bold();
+    popUp.style.display = "flex";
 }
+
+function registrar(){
+    if(user.value === "" || password.value === ""){
+        alert("Porfavor llene todos los campos");
+        return;
+    }
+
+    msg.innerHTML = "Registro exitoso " + (user.value).bold();
+    popUp.style.display = "flex";
+}
+
+function emptyFields(){
+    user.value = "";
+    password.value = "";
+    msg.innerHTML = "";
+    popUp.style.display = "none";
+}   
+
+loginBtn.addEventListener('click', iniciar);
+signUpBtn.addEventListener('click', registrar);
+continueBtn.addEventListener('click', emptyFields);
